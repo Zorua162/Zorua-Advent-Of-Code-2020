85434e8a9b593aa016b5c49c53b103912e610bbe
Zorua162 <jakeirwin03@gmail.com>
2020-12-24 01:37:33 +0000

    started day 5

--- day5/seat_finder.py
+++ day5/seat_finder.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+from math import ceil
+from math import floor
 
 with open('./seat_data.txt','r') as f:
     data = f.read().splitlines()
@@ -8,27 +10,35 @@ with open('./seat_data.txt','r') as f:
 
 
 
-def next_position(current_row, curent_column, ,row_pointer, column_pointer,
+def next_position(current_row, curent_column,
                   next_letter):
-    if next_letter = 'B':
-        current_row = curent_column/2
-    elif next_letter = 'F':
-        current_row =
+    print(next_letter)
+    if next_letter == 'F':
+        current_row = current_row[:ceil(len(current_row)/2)]
+    elif next_letter == 'B':
+        current_row = current_row[int(len(current_row)/2):]
+    elif next_letter == 'L':
+        current_column = curent_column[:int(len(curent_column)/2)]
+    elif next_letter == 'R':
+        current_column = curent_column[int(len(curent_column)/2):]
+    return current_row, curent_column
+
+def get_seat_pos(seat):
+    current_row = [i for i in range(127)]
+    curent_column = [i for i in range(7)]
+    for pos in list(seat):
+        current_row, current_column = next_position(current_row, curent_column,
+                                                    pos)
+        print(min(current_row), max(current_row),   min(curent_column), max(curent_column))
     return current_row, curent_column
 
 def main():
     for seat in data:
-        current_row = range(127)
-        curent_column = range(7)
-        row_pointer = [0, 127]
-        column_pointer = [0, 7]
-        for pos in list(seat):
-            current_row, current_row = next_position(current_row,
-                                                     curent_column,
-                                                     row_pointer,
-                                                     column_pointer,
-                                                     next_letter)
+        row, column = get_seat_pos(seat)
 
+test_seat = "BFFFBBFRRR"
 
-
-main()
+print(get_seat_pos(test_seat))
+print('------------------first seven below -------------------')
+print(get_seat_pos('FBFBBFFRLR'))
+#main()
